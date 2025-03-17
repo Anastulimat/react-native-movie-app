a110a9a8607c08c5de5c2e14d58bddecc3e1f7e4
Anas TULIMAT <anas.tulimat@orange.com>
2025-03-17 16:44:36 +0100

    first commit

---
+++ app/movies/[id].tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+const MovieDerails
+    = () => {
+    return (
+        <View>
+            <Text>MovieDerails</Text>
+        </View>
+    )
+}
+
+export default MovieDerails;
+const styles = StyleSheet.create({})
