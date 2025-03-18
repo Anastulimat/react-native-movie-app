e328035e7d5f30dc01dfe6af8af4af2eb4dcd808
Anas TULIMAT <anas.tulimat@orange.com>
2025-03-18 11:47:36 +0100

    feat: add tranding movies section to the home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,27 @@
+import {ActivityIndicator, FlatList, Image, ScrollView, Text, View,} from "react-native";
 import {useRouter} from "expo-router";
-import {ActivityIndicator, Image, ScrollView, View, Text, FlatList} from "react-native";
+
+import useFetch from "@/services/useFetch";
+import {fetchMovies} from "@/services/api";
+import {getTrendingMovies} from "@/services/appwrite";
 
 import {icons} from "@/constants/icons";
 import {images} from "@/constants/images";
-import SearchBar from "@/components/SearchBar";
 
-import {fetchMovies} from "@/services/api";
-import useFetch from "@/services/useFetch";
+import SearchBar from "@/components/SearchBar";
 import MovieCard from "@/components/MovieCard";
+import TrendingCard from "@/components/TrendingCard";
 
 // ----------------------------------------------------------------------
 
-export default function Index() {
+const Index = () => {
     const router = useRouter();
+
+    const {
+        data: trendingMovies,
+        loading: trendingLoading,
+        error: trendingError,
+    } = useFetch(getTrendingMovies);
 
     const {
         data: movies,
@@ -25,31 +34,26 @@ export default function Index() {
             <Image
                 source={images.bg}
                 className="absolute w-full z-0"
+                resizeMode="cover"
             />
 
             <ScrollView
                 className="flex-1 px-5"
                 showsVerticalScrollIndicator={false}
-                contentContainerStyle={{
-                    minHeight: "100%",
-                    paddingBottom: 10
-                }}
+                contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}
             >
-                <Image
-                    source={icons.logo}
-                    className="w-12 h-10 mt-20 mb-5 mx-auto"
-                />
+                <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
 
-                {moviesLoading ? (
+                {moviesLoading || trendingLoading ? (
                     <ActivityIndicator
                         size="large"
                         color="#0000ff"
                         className="mt-10 self-center"
                     />
-                ) : moviesError ? (
-                    <Text>Error: {moviesError?.message}</Text>
+                ) : moviesError || trendingError ? (
+                    <Text>Error: {moviesError?.message || trendingError?.message}</Text>
                 ) : (
-                    <View className="flex-1 pt-5">
+                    <View className="flex-1 mt-5">
                         <SearchBar
                             onPress={() => {
                                 router.push("/search");
@@ -57,16 +61,36 @@ export default function Index() {
                             placeholder="Search for a movie"
                         />
 
+                        {trendingMovies && (
+                            <View className="mt-10">
+                                <Text className="text-lg text-white font-bold mb-3">
+                                    Trending Movies
+                                </Text>
+                                <FlatList
+                                    horizontal
+                                    showsHorizontalScrollIndicator={false}
+                                    className="mb-4 mt-3"
+                                    data={trendingMovies}
+                                    contentContainerStyle={{
+                                        gap: 26,
+                                    }}
+                                    renderItem={({item, index}) => (
+                                        <TrendingCard movie={item} index={index}/>
+                                    )}
+                                    keyExtractor={(item) => item.movie_id.toString()}
+                                    ItemSeparatorComponent={() => <View className="w-4"/>}
+                                />
+                            </View>
+                        )}
+
                         <>
-                            <Text
-                                className="text-lg text-white font-bold mt-5 mb-3"
-                            >
-                                Latest movies
+                            <Text className="text-lg text-white font-bold mt-5 mb-3">
+                                Latest Movies
                             </Text>
 
                             <FlatList
                                 data={movies}
-                                renderItem={({ item }) => <MovieCard {...item} />}
+                                renderItem={({item}) => <MovieCard {...item} />}
                                 keyExtractor={(item) => item.id.toString()}
                                 numColumns={3}
                                 columnWrapperStyle={{
@@ -81,8 +105,9 @@ export default function Index() {
                         </>
                     </View>
                 )}
-
             </ScrollView>
         </View>
     );
-}
+};
+
+export default Index;
